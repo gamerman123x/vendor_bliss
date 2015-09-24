f6df8821dfa701cd86869c53a756f55d10fd0a6a
gamerman123x <19superguy98@gmail.com>
2015-09-23 23:47:15 -0400

    Fix
    Change-Id: Iaf1205bf2bc8e6fcc844741cd305ecb914390b94

--- config/common.mk
+++ config/common.mk
@@ -108,10 +108,6 @@ PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/bliss/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
-# Blissful Wallpapers
-PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/blisspapers/BlissPapers.apk:system/app/BlissPapers/BlissPapers.apk
-
 # Bliss-specific init file
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/etc/init.local.rc:root/init.cm.rc
